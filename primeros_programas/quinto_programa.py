86202045918335057d1a19df69ea27f2c309f1e1
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-08-24 08:56:52 -0500

    Create quinto_programa.py

---
+++ primeros_programas/quinto_programa.py
@@ -0,0 +1,12 @@
+coffe_price = 1.50
+number_of_coffes = 4
+
+print (coffe_price * number_of_coffes)
+print (coffe_price)
+print (number_of_coffes)
+
+coffe_price = 2.00
+
+print(coffee_price * number_of_coffees)
+print(coffee_price)
+print(number_of_coffees)
