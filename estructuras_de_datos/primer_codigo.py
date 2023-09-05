16edc6d168951db40a5b016984a3a3105f774188
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-09-05 12:45:35 -0500

    Update primer_codigo.py

--- estructuras_de_datos/primer_codigo.py
+++ estructuras_de_datos/primer_codigo.py
@@ -1,19 +1,19 @@
-a = input("Enter a number: ")
+a = input("ingresa un numero a: ")
 a = int(a)
-b = input("Enter b number: ")
+b = input("ingresa un numero b: ")
 b = float(b)
 c = a + b
 
 if a == b:
-    print("equal")
+    print("iguales")
 else:
-    print("Different")
+    print("Diferentes")
 
-print("Type of a is: ", type(a))
-print("Type of b is: ", type(b))
+print("a es tipo: ", type(a))
+print("b es tipo: ", type(b))
 print("c = ", c)
 
 if type(a) == type(b):
-    print("a and b are of the same type")
+    print("a y b son del mismo tipo")
 else:
-    print("a and b are of different type")
+    print("a y b son de  de diferntes tipos")
