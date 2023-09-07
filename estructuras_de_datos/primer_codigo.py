4b0cc1baf56681b00854a3b4c248a128346cad09
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-09-07 12:48:17 -0500

    Update primer_codigo.py

--- estructuras_de_datos/primer_codigo.py
+++ estructuras_de_datos/primer_codigo.py
@@ -1,19 +1,100 @@
-a = input("ingresa un numero a: ")
-a = int(a)
-b = input("ingresa un numero b: ")
-b = float(b)
-c = a + b
+#################LISTAS####################
+###########################################
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+#input()
+print(my_lista)
+print(type(my_lista))
+print(my_lista[2])
 
-if a == b:
-    print("iguales")
-else:
-    print("Diferentes")
+print("my_lista size: ", len(my_lista))
+print(my_lista[0:2])
+print(my_lista[:2])
 
-print("a es tipo: ", type(a))
-print("b es tipo: ", type(b))
-print("c = ", c)
+my_lista.append('Blanco')      #Agrega elemento al final de la lista
+print(my_lista)
 
-if type(a) == type(b):
-    print("a y b son del mismo tipo")
-else:
-    print("a y b son de  de diferntes tipos")
+my_lista.insert(3, 'Negro')
+print(my_lista)
+
+
+my_lista.extend(['Marron', 'Gris'])   #Concatena a otra lista
+print(my_lista)
+
+print(my_lista.index('Azul'))
+
+#my_lista.remove('Magenta')
+my_lista.remove('Marron')
+print(my_lista)
+
+my_lista.insert(8, 'Marron')
+print(my_lista)
+
+print(my_lista.pop())
+size = len(my_lista)
+print("size = ", size)
+#print(my_lista.pop(size))
+
+my_lista_3 = my_lista*3
+print("my_lista_3: ", my_lista_3)
+
+print("Sort:")
+print()
+my_listaSort = my_lista.sort()
+print(my_listaSort)
+
+my_NumList = [10, 9, 8, 7, 6 , 5 , 4, 3, 2, 1]
+print("Ordering my_NumList: ")
+my_NumList.sort()
+print(my_NumList)
+#OrderedLList = my_NumList.sort()
+#print(my_listaSort)
+
+#Ordenando lista de mayor a menor
+my_NumList.sort(reverse = True)
+print("De menor a mayor: ", my_NumList)
+
+
+
+#################TUPLAS####################
+###########################################
+# Corresponde a una estructura similar a las listas, la diferencia está
+# en que no se pueden modificar una vez creadas, es decir que son inmutables:
+
+#Convertir una lista a tupla:prin
+print("###########################")
+print("###########################")
+print("###########################")
+print("############TUPLAS#########")
+my_tupla = tuple(my_lista)
+print()
+print()
+print("my_tuple: ", my_tupla)
+
+print(my_tupla[0])
+print(my_tupla[2])
+
+
+#Evaluar si un elemento está contenido en la tupla (Devuelve un valor booleano)
+print('Rojo' in my_tupla)
+print(my_tupla.count('Rojo'))
+
+#Tupla con un solo elemento
+my_tupla_unitaria = ('Blanco')
+print(my_tupla_unitaria)
+
+#Empaquetado de tupla, tupla sin paréntesis
+my_tupla = 'Gaspar', 5, 8, 1999
+print(my_tupla)
+
+#Desempaquetado de tupla, se guardan los valores en orden de las variables
+nombre, dia, mes, año = my_tupla
+print(nombre)
+print(dia)
+print(mes)
+print(año)
+
+print("Nombre: ", nombre, " - Dia:", dia, " - Mes: ", mes, "- Año: ", año)
+
+#Convertir una tupla en una lista
+my_lista2=list(my_tupla)
+print(my_lista2)
