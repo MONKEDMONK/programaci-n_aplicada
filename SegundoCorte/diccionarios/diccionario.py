40bca1c2769ac1576e1f7ddf1602467e384db527
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-09-12 12:59:36 -0500

    Update diccionario.py

--- SegundoCorte/diccionarios/diccionario.py
+++ SegundoCorte/diccionarios/diccionario.py
@@ -1,1 +1,16 @@
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+print(sensors )
+print(sensors)
+
+translations = {"mountain":	"orod", "bread":	"bass", "friend":	"mellon", "horse":	"roch" }
+print(translations)
+
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"] , "Corleone": ["Sonny", "Fredo", "Michael"],20211005044: 15}
+print (children)
+
+vacio = {"diomedes":3}
+vacio ["mancos" ] = 4
+print (vacio)
+
 
