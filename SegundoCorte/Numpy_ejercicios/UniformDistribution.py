93e60bf8b9c16d56e1d752e5d56c924b0a3122ee
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-16 08:42:06 -0500

    Create UniformDistribution.py

---
+++ SegundoCorte/Numpy_ejercicios/UniformDistribution.py
@@ -0,0 +1,15 @@
+##example 1##
+from numpy import random
+
+x = random.uniform(size=(2, 3))
+
+print(x)
+## Visualization of Uniform Distribution##
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.uniform(size=1000), hist=False)
+
+plt.show()
