1b468c1fb6c44d19d166acd7223bafe42d952065
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-16 13:40:26 -0500

    Update Matplotlib_Line.py

--- SegundoCorte/Matplotlib_Ejercicios/Matplotlib_Line.py
+++ SegundoCorte/Matplotlib_Ejercicios/Matplotlib_Line.py
@@ -13,3 +13,43 @@ plt.plot(ypoints, linestyle = 'dashed')
 ##Shorter Syntax##
 ## ejemplo##
 plt.plot(ypoints, ls = ':')
+
+            #####Line Styles###
+'solid' (default)	'-'	
+'dotted'	':'	
+'dashed'	'--'	
+'dashdot'	'-.'	
+'None'	'' or ' '
+              ##line color##
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, color = 'r')
+plt.show()
+
+###Multiple_Lines##
+##ejemplo##
+import matplotlib.pyplot as plt
+import numpy as np
+
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
+### ejemplo 2###
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1, x2, y2)
+plt.show()
