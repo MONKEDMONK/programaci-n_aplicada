972d8b69e3304ef86bfc07348373f6bdd27b8bca
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-16 08:50:13 -0500

    Create Rayleigh_Distribution.py

---
+++ SegundoCorte/Numpy_ejercicios/Rayleigh_Distribution.py
@@ -0,0 +1,15 @@
+##ejemplo1##
+from numpy import random
+
+x = random.rayleigh(scale=2, size=(2, 3))
+
+print(x)
+
+##Visualization of Rayleigh Distribution##
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.rayleigh(size=1000), hist=False)
+
+plt.show()
