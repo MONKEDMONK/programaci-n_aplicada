ea0156bc25f2ffd3425f0e52956721b9a01dc5bc
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-16 08:36:20 -0500

    Update Binomial_Distribution.py

--- SegundoCorte/Numpy_ejercicios/Binomial_Distribution.py
+++ SegundoCorte/Numpy_ejercicios/Binomial_Distribution.py
@@ -1,1 +1,24 @@
+  ##ejempo1##
+from numpy import random
 
+x = random.binomial(n=10, p=0.5, size=10)
+
+print(x)
+## visualization of binomial Distribution##
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, kde=False)
+
+plt.show()
+
+##Difference Between Normal and Binomial Distribution##
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.normal(loc=50, scale=5, size=1000), hist=False, label='normal')
+sns.distplot(random.binomial(n=100, p=0.5, size=1000), hist=False, label='binomial')
+
+plt.show()
