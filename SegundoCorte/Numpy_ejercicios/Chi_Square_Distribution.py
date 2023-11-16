a145dc0d391b3ad9fa8e5ca8f3a8068615a3d2da
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-16 08:48:42 -0500

    Create Chi_Square_Distribution.py

---
+++ SegundoCorte/Numpy_ejercicios/Chi_Square_Distribution.py
@@ -0,0 +1,14 @@
+##ejemplo1##
+from numpy import random
+
+x = random.chisquare(df=2, size=(2, 3))
+
+print(x)
+##Visualization of Chi Square Distribution##
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.chisquare(df=1, size=1000), hist=False)
+
+plt.show()
