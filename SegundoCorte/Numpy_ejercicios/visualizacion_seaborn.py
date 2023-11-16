d4e0d2e410bd027c397c3ef0d6036b0cf92702a9
MONKEDMONK <142928715+MONKEDMONK@users.noreply.github.com>
2023-11-15 23:33:34 -0500

    Create visualizacion_seaborn.py

---
+++ SegundoCorte/Numpy_ejercicios/visualizacion_seaborn.py
@@ -0,0 +1,22 @@
+      ##Import Matplotlib##
+import matplotlib.pyplot as plt
+
+      ##Import Seaborn##
+import seaborn as sns
+
+    ##Plotting a Distplot##
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot([0, 1, 2, 3, 4, 5])
+
+plt.show()
+
+    ##Plotting a Distplot Without the Histogram##
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
+
+plt.show()
